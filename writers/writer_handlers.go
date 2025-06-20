a3ed6e20115402aa9bf92ea0cf13485024490442
Sayanjit Mukherjee <silverfangthegem@gmail.com>
2025-06-20 16:20:42 +0530

    fix: fixing error handling.
    Signed-off-by: Sayanjit Mukherjee <silverfangthegem@gmail.com>

--- writers/writer_handlers.go
+++ writers/writer_handlers.go
@@ -2,37 +2,54 @@ package writers
 
 import (
 	"encoding/csv"
+	"errors"
 	"fmt"
-	"log"
 	"os"
 )
 
-//"encoding/csv"
+// "encoding/csv"
+func Exist_check(path string) (error) {
 
-func Create_csv(filename string, fields []string) { //path of file , fields slice 
-	f, err := os.OpenFile(fmt.Sprintf("./db/%v",filename), os.O_CREATE|os.O_WRONLY, 0644)
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		return errors.New("file does not exist")
+	} else if err != nil {
+		return fmt.Errorf("error checking file: %v", err)
+	} 
+
+	return nil
+}
+
+func Create_csv(filename string, fields []string) error { //path of file , fields slice
+	f, err := os.OpenFile(fmt.Sprintf("./db/%v", filename), os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error message : %v", err)
 	}
 
 	defer f.Close()
 
-	w := csv.NewWriter(f)  //accepts the io.writer but in this case I have passed *os.file
+	w := csv.NewWriter(f) //accepts the io.writer but in this case I have passed *os.file
 
 	w.Write(fields)
 	w.Flush()
 
-	if err:= w.Error() ; err != nil{
-		log.Fatalln("Error message : ",err)
+	if err := w.Error(); err != nil {
+		return fmt.Errorf("error message : %v", err)
 	}
+
+	return nil
 }
 
+func Add_data(filename string, fields [][]string) error{
 
+	err  := Exist_check(fmt.Sprintf("./db/%v",filename))
 
-func Add_data(filename string, fields [][]string) {
-	f, err := os.OpenFile(fmt.Sprintf("./db/%v",filename), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("the error is: %v",err)
+	}
+
+	f, err := os.OpenFile(fmt.Sprintf("./db/%v", filename), os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("error message : %v", err)
 	}
 
 	defer f.Close()
@@ -41,7 +58,9 @@ func Add_data(filename string, fields [][]string) {
 
 	w.WriteAll(fields)
 
-	if err:= w.Error() ; err != nil{
-		log.Fatalln("Error message : ",err)
+	if err := w.Error(); err != nil {
+		return fmt.Errorf("error message : %v", err)
 	}
+
+	return nil
 }
